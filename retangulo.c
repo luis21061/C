481e347c72c54892cb89dfed7802a95a07b818f0
Luis Magalhães <52885705+luis21061@users.noreply.github.com>
2022-07-09 11:49:02 -0300

    Add files via upload

---
+++ retangulo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    double base, altura, area, perimetro, diagonal;
+
+    printf("Base do retangulo: ");
+    scanf("%lf", &base);
+    printf("Altura do retangulo: ");
+    scanf("%lf", &altura);
+
+    area = base * altura;
+    printf("AREA = %.4lf\n", area);
+
+    perimetro = 2.0*(base + altura);
+    printf("PERIMETRO = %.4lf\n", perimetro);
+
+    diagonal = sqrt(pow(base,2)+pow(altura,2));
+    printf("DIAGONAL = %.4lf\n", diagonal);
+
+return 0;
+}
